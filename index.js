d196e023585fcd7d74c7992766fc711d4508a483
GiffenGood1 <matthewstyzinski@gmail.com>
2021-02-24 15:19:22 +1100

    changed to async/await

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const apiKey = "ENTER OWN API KEY HERE"; // ENTER OWN API KEY HERE
+const apiKey = "047e6d405727c5888f41771c6e262647"; // ENTER OWN API KEY HERE
 
-const makeCard = (icon, name, description, temp, feelsLike) => {
+const makeCard = (icon, name, description, temp, feelsLike, max, min) => {
   const card = document.querySelector("#weatherCard");
   card.innerHTML = `
 <div class="card">
@@ -10,37 +10,26 @@ const makeCard = (icon, name, description, temp, feelsLike) => {
       <h6><img src='http://openweathermap.org/img/wn/${icon}.png'>${description}</h6>
     </div>
     <div class="col">
-      <h6 class="text-end">${temp}C, feels like ${feelsLike}C</h6>
+      <h6 class="text-end">it's currently ${temp}C,</h6>
+      <h6 class="text-end">but it feels like ${feelsLike}C</h6>
+      <br>
+      <h6 class="text-end">todays max is: ${max}C,</h6>
+      <h6 class="text-end">todays min is: ${min}C,</h6>
     </div>
   </div>
 </div>`;
 };
 
-const getWeatherData = (city) =>
-  fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      const temp = data.main.temp;
-      const feelsLike = data.main["feels_like"];
-      const name = data.name;
-      const description = data.weather[0].description;
-      const icon = data.weather[0].icon;
-
-      makeCard(icon, name, description, temp, feelsLike);
-    })
-    .catch((error) => console.log("error"));
-
 const button = document.querySelector("button");
+const locationBtn = document.querySelector("#location-btn");
 
+// Go button event
 button.addEventListener("click", () => {
   const input = document.querySelector("input");
   getWeatherData(input.value);
 });
 
-const locationBtn = document.querySelector("#location-btn");
-
+//Location button event
 locationBtn.addEventListener("click", () => {
   const locationIcon = document.querySelector("#location-icon");
   if (locationIcon.classList.contains("bi-geo-alt-fill")) {
@@ -59,19 +48,53 @@ locationBtn.addEventListener("click", () => {
     );
   }
 });
+// Fetch API call by latitude and longitude
+const getWeatherDataByLocation = async (lat, lon) => {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
+    );
+    //convert response into data
+    const data = await response.json();
+    console.log(data);
+    // Grab needed data
+    const temp = data.main.temp;
+    const feelsLike = data.main["feels_like"];
+    const name = data.name;
+    const description = data.weather[0].description;
+    const icon = data.weather[0].icon;
+    const min = data.main["temp_min"];
+    const max = data.main["temp_max"];
 
-const getWeatherDataByLocation = (lat, lon) =>
-  fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      const temp = (data.main.temp - 273.15).toFixed(2); // converting Kelvin to Celcius
-      const feelsLike = (data.main["feels_like"] - 273.15).toFixed(2); // converting Kelvin to Celcius
-      const name = data.name;
-      const description = data.weather[0].description;
-      const icon = data.weather[0].icon;
+    makeCard(icon, name, description, temp, feelsLike, max, min);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-      makeCard(icon, name, description, temp, feelsLike);
-    })
-    .catch((error) => console.log("error"));
+// Fetch API call by city name
+const getWeatherData = async (city) => {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+    );
+
+    if (!response.ok) {
+      const card = document.querySelector("#weatherCard");
+      card.innerHTML = `<h4>HTTP error! status: ${response.status}</h4>`;
+    }
+
+    const data = await response.json();
+    console.log(data);
+    const temp = data.main.temp;
+    const feelsLike = data.main["feels_like"];
+    const name = data.name;
+    const description = data.weather[0].description;
+    const icon = data.weather[0].icon;
+    const min = data.main["temp_min"];
+    const max = data.main["temp_max"];
+    makeCard(icon, name, description, temp, feelsLike, max, min);
+  } catch (error) {
+    console.log(error);
+  }
+};
