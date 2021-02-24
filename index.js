39cbec500edd18b3f2af0778a20c377e1dbe1860
GiffenGood1 <matthewstyzinski@gmail.com>
2021-02-24 11:27:59 +1100

    added location button

--- index.js
+++ index.js
@@ -7,11 +7,10 @@ const makeCard = (icon, name, description, temp, feelsLike) => {
   <div class="row m-4">
     <div class="col">
       <h4 class="card-title">${name}</h5>
-      <h6>${description}</h6>
+      <h6><img src='http://openweathermap.org/img/wn/${icon}.png'>${description}</h6>
     </div>
     <div class="col">
       <h6 class="text-end">${temp}C, feels like ${feelsLike}C</h6>
-      <img src='http://openweathermap.org/img/wn/${icon}.png'>
     </div>
   </div>
 </div>`;
@@ -39,3 +38,40 @@ button.addEventListener("click", () => {
   const input = document.querySelector("input");
   getWeatherData(input.value);
 });
+
+const locationBtn = document.querySelector("#location-btn");
+
+locationBtn.addEventListener("click", () => {
+  const locationIcon = document.querySelector("#location-icon");
+  if (locationIcon.classList.contains("bi-geo-alt-fill")) {
+    locationIcon.classList.toggle("bi-geo-alt-fill");
+    locationIcon.classList.toggle("bi-geo-alt");
+  } else if (locationIcon.classList.contains("bi-geo-alt")) {
+    locationIcon.classList.toggle("bi-geo-alt-fill");
+    locationIcon.classList.toggle("bi-geo-alt");
+    // get current location
+    const currentLocation = navigator.geolocation.getCurrentPosition(
+      (geoPosition) => {
+        const lat = geoPosition.coords.latitude;
+        const lon = geoPosition.coords.longitude;
+        getWeatherDataByLocation(lat, lon);
+      }
+    );
+  }
+});
+
+const getWeatherDataByLocation = (lat, lon) =>
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const temp = (data.main.temp - 273.15).toFixed(2); // converting Kelvin to Celcius
+      const feelsLike = (data.main["feels_like"] - 273.15).toFixed(2); // converting Kelvin to Celcius
+      const name = data.name;
+      const description = data.weather[0].description;
+      const icon = data.weather[0].icon;
+
+      makeCard(icon, name, description, temp, feelsLike);
+    })
+    .catch((error) => console.log("error"));
